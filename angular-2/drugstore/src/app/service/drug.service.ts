714753ad74de1edca007ca2dcedf4764f9accbd4
BKMarcsi <89334602+BKMarcsi@users.noreply.github.com>
2022-02-28 15:46:50 +0100

    feladat 2_2

--- angular-2/drugstore/src/app/service/drug.service.ts
+++ angular-2/drugstore/src/app/service/drug.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Drug } from '../model/drug';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DrugService {
 
-  constructor() { }
+  apiUrl: string = environment.apiUrl;
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  getAll(): Observable<Drug[]>{
+    return this.http.get<Drug[]>(`${this.apiUrl}`)
+  }
 }
