113b2297c17c5bbc357f4eb03d6300ae251000a4
BKMarcsi <89334602+BKMarcsi@users.noreply.github.com>
2022-02-28 14:47:39 +0100

    feladat 5

---
+++ firebase-5/src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAtItjb8vfQDobSoueu3qgOCCPDVuiD1pc",
+  authDomain: "bkmarcsi-firebase-5.firebaseapp.com",
+  projectId: "bkmarcsi-firebase-5",
+  storageBucket: "bkmarcsi-firebase-5.appspot.com",
+  messagingSenderId: "26590797823",
+  appId: "1:26590797823:web:9838d5340f752e32a5d1ca",
+  measurementId: "G-16104P3Y8H"
+};
+
+export const environment = {
+  production: true,
+  firebaseConfig,
+};
