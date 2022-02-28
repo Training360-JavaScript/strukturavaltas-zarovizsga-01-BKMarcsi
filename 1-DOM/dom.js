e016e8c4e8feecd6fb3c94ad44513853830f19e0
BKMarcsi <89334602+BKMarcsi@users.noreply.github.com>
2022-02-28 15:08:14 +0100

    feladat 1_2

--- 1-DOM/dom.js
+++ 1-DOM/dom.js
@@ -1,5 +1,10 @@
 function manipulateDom() {
-    // 
+    const domArray = document.getElementsByClassName('quote');
+    for (let i=0;i<domArray.length;i++){
+        domArray[i].style.borderColor="blue";
+        domArray[i].style.backgroundColor = "lightblue";
+        domArray[i].style.fontStyle = "italic";
+    };
 };
 
 export { manipulateDom };
